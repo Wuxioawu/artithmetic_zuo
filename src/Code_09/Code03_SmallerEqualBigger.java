6e98630ec81444c3d862ebcce2e0fb85b1032b08
wu peng <pen_warren@192.168.1.21>
2025-05-19 17:14:10 +0800

    fix some question about the linkedList

--- src/Code_09/Code03_SmallerEqualBigger.java
+++ src/Code_09/Code03_SmallerEqualBigger.java
@@ -1,4 +1,180 @@
 package Code_09;
 
+import Code_03.SingleListOperation;
+import Code_03.SingleNode;
+import tools.Constants;
+import tools.NumberOperation;
+
+import java.util.ArrayList;
+
 public class Code03_SmallerEqualBigger {
+
+    public static SingleNode partition(SingleNode head, int value) {
+        if (head == null || head.next == null) return head;
+
+        SingleNode lessHead = null;
+        SingleNode lessTail = null;
+
+        SingleNode equalHead = null;
+        SingleNode equalTail = null;
+
+        SingleNode greaterHead = null;
+        SingleNode greaterTail = null;
+
+        SingleNode current = head;
+
+        while (current != null) {
+            SingleNode next = current.next;
+
+            SingleNode operationHead;
+            SingleNode operationTail;
+
+            if (current.value < value) {
+                operationHead = lessHead;
+                operationTail = lessTail;
+            } else if (current.value == value) {
+                operationHead = equalHead;
+                operationTail = equalTail;
+            } else {
+                operationHead = greaterHead;
+                operationTail = greaterTail;
+            }
+
+            if (operationHead == null) {
+                operationHead = current;
+            } else {
+                operationTail.next = current;
+            }
+
+            operationTail = current;
+
+            current.next = null;
+            current = next;
+        }
+
+        if (lessTail != null) {
+            lessTail.next = equalHead;
+            equalTail = equalTail == null ? lessTail : equalHead;
+        }
+
+        if (equalTail != null) {
+            equalTail.next = greaterHead;
+        }
+
+        return lessTail == null ? equalHead : lessHead;
+    }
+
+    public static SingleNode partitionByList(SingleNode head, int value) {
+        if (head == null || head.next == null) return head;
+
+        ArrayList<SingleNode> lessNodes = new ArrayList<>();
+        ArrayList<SingleNode> equalNodes = new ArrayList<>();
+        ArrayList<SingleNode> greaterNodes = new ArrayList<>();
+
+        SingleNode next = head;
+        while (next != null) {
+            SingleNode current = next;
+            next = next.next;
+            if (current.value == value) {
+                equalNodes.add(current);
+            } else if (current.value < value) {
+                lessNodes.add(current);
+            } else {
+                greaterNodes.add(current);
+            }
+            current.next = null;
+        }
+
+        lessNodes.addAll(equalNodes);
+        lessNodes.addAll(greaterNodes);
+
+        for (int i = 0; i < lessNodes.size() - 1; i++) {
+            lessNodes.get(i).next = lessNodes.get(i + 1);
+        }
+        return lessNodes.getFirst();
+    }
+
+    //for test
+    public static boolean isPartitionRight(SingleNode head, int value) {
+        if (head == null || head.next == null) return true;
+
+        SingleNode current = head;
+
+        while (current != null) {
+            if (current.value == value) {
+                break;
+            }
+            current = current.next;
+        }
+
+        while (current != null) {
+            if (current.value > value) {
+                break;
+            }
+
+            if (current.value < value) {
+                return false;
+            }
+            current = current.next;
+        }
+
+        while (current != null) {
+            if (current.value <= value) {
+                return false;
+            }
+            current = current.next;
+        }
+
+        return true;
+    }
+
+    public static int randomValueFromList(SingleNode head) {
+        if (head == null) {
+            return -1;
+        }
+        if (head.next == null) {
+            return head.value;
+        }
+
+        SingleNode next = head;
+        int index = 0;
+        while (next != null) {
+            next = next.next;
+            index++;
+        }
+        int randomStep = NumberOperation.getRandomNumber(index);
+        next = head;
+        for (int i = 0; next.next != null && i < randomStep; i++) {
+            next = next.next;
+        }
+
+        return next.value;
+
+    }
+
+    public static void test() {
+        System.out.println(Constants.START_TEST);
+
+        for (int i = 0; i < Constants.TEST_TIMES; i++) {
+            SingleNode head = SingleListOperation.getRandomLinkedList(10, 100);
+            int value = randomValueFromList(head);
+
+            SingleNode partition1 = partition(head, value);
+//            SingleNode partition2 = partitionByList(head, value);
+
+            if (!isPartitionRight(partition1, value)) {
+                SingleListOperation.printSingleList(head);
+                System.out.println(value);
+                SingleListOperation.printSingleList(partition1);
+                System.out.println(Constants.CODE_ERROR);
+                return;
+            }
+        }
+
+        System.out.println(Constants.TEST_FINISH);
+    }
+
+    public static void main(String[] args) {
+        test();
+    }
 }
--- src/Code_09/Code04_CopyListWithRandom.java
+++ src/Code_09/Code04_CopyListWithRandom.java
@@ -1,4 +1,184 @@
 package Code_09;
 
+import tools.Constants;
+import tools.NumberOperation;
+
+import java.util.ArrayList;
+
 public class Code04_CopyListWithRandom {
+
+    public static class Node {
+        int value;
+        Node next;
+        Node rand;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static Node copyRandomList(Node head) {
+        if (head == null) return null;
+        if (head.next == null) return new Node(head.value);
+
+        Node current = head;
+        while (current != null) {
+            Node next = current.next;
+            Node node = new Node(current.value);
+            current.next = node;
+            node.next = next;
+            current = next;
+        }
+
+        current = head;
+        Node current_new = head.next;
+
+        while (current_new != null) {
+            if (current.rand != null) {
+                current_new.rand = current.rand.next;
+            }
+            current = current_new.next;
+            current_new = current != null ? current.next : null;
+        }
+
+        current = head;
+        current_new = head.next;
+        Node newHead = current_new;
+
+        while (current_new != null) {
+            current.next = current_new.next;
+            current = current.next;
+
+            current_new.next = current != null ? current.next : null;
+            current_new = current_new.next;
+        }
+        return newHead;
+    }
+
+    public static Node copyRandomListByCollection(Node head) {
+        if (head == null) return null;
+        if (head.next == null) return new Node(head.value);
+
+        ArrayList<Node> list = new ArrayList<>();
+        Node cur = head;
+        while (cur != null) {
+            list.add(cur);
+            cur = cur.next;
+        }
+
+        ArrayList<Node> newList = new ArrayList<>();
+        newList.add(new Node(list.getFirst().value));
+        for (int i = 1; i < list.size(); i++) {
+            newList.add(new Node(list.get(i).value));
+            newList.get(i - 1).next = newList.get(i);
+        }
+
+        for (int i = 0; i < newList.size(); i++) {
+            if (list.get(i).rand != null) {
+                int index = list.indexOf(list.get(i).rand);
+                newList.get(i).rand = newList.get(index);
+            }
+        }
+
+        return newList.getFirst();
+    }
+
+    //for test
+    public static Node getRandomList(int maxSize, int maxValue) {
+        int size = NumberOperation.getRandomNumber(maxSize);
+        int value = NumberOperation.getRandomNumber(maxValue);
+
+        Node head = new Node(NumberOperation.getRandomNumber(maxValue));
+        Node node = new Node(NumberOperation.getRandomNumber(maxValue));
+
+        head.next = node;
+
+        for (int i = 0; i < size; i++) {
+            node.next = new Node(NumberOperation.getRandomNumber(value));
+            node = node.next;
+        }
+
+        int modifyTime = NumberOperation.getRandomNumber(size);
+
+        ArrayList<Node> nodes = new ArrayList<>();
+        Node current = head;
+        while (current != null) {
+            nodes.add(current);
+            current = current.next;
+        }
+
+        for (int i = 0; i < modifyTime; i++) {
+            int left, right;
+            do {
+                left = NumberOperation.getRandomNumber(nodes.size());
+                right = NumberOperation.getRandomNumber(nodes.size());
+            } while (left == right);
+
+            nodes.get(left).rand = nodes.get(right);
+        }
+
+        return head;
+    }
+
+    public static boolean isEqual(Node head1, Node head2) {
+        if (head1 == null && head2 == null) return true;
+
+        ArrayList<Node> nodes1 = new ArrayList<>();
+        ArrayList<Node> nodes2 = new ArrayList<>();
+
+        Node current = head1;
+        while (current != null) {
+            nodes1.add(current);
+            current = current.next;
+        }
+
+        current = head2;
+        while (current != null) {
+            nodes2.add(current);
+            current = current.next;
+        }
+
+        if (nodes1.size() != nodes2.size()) return false;
+
+        for (int i = 0; i < nodes1.size(); i++) {
+            if (nodes1.get(i).value != nodes2.get(i).value) return false;
+            if (nodes1.get(i).rand == null && nodes2.get(i).rand == null) continue;
+            if (nodes1.get(i).rand == null || nodes2.get(i).rand == null) return false;
+            if (nodes1.get(i).rand.value != nodes2.get(i).rand.value) return false;
+        }
+        return true;
+    }
+
+    public static void printList(Node head) {
+        if (head == null) return;
+        Node current = head;
+        while (current != null) {
+            String str = current.rand != null ? " ->rand:" + current.rand.value : " ";
+            System.out.print(" {" + current.value + str + "} -> ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void test() {
+        System.out.println(Constants.START_TEST);
+
+        for (int i = 0; i < Constants.TEST_TIMES; i++) {
+            Node head = getRandomList(10, 100);
+            Node copy = copyRandomList(head);
+
+            if (!isEqual(head, copy)) {
+                printList(head);
+                printList(copy);
+                System.out.println(Constants.CODE_ERROR);
+                return;
+            }
+        }
+        System.out.println(Constants.TEST_FINISH);
+    }
+
+    public static void main(String[] args) {
+        test();
+    }
+
 }
