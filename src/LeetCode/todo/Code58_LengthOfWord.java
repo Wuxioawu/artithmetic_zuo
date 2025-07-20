65052ac9645859d56a8b00b0de5bfbc65d1ed043
wu peng <1761230580@qq.com>
2025-07-20 22:31:10 +0100

    leetcode 58

---
+++ src/LeetCode/todo/Code58_LengthOfWord.java
@@ -0,0 +1,29 @@
+package LeetCode.todo;
+
+public class Code58_LengthOfWord {
+
+    public static int lengthOfLastWord(String s) {
+        int lastWordLength = 0;
+        char[] charArray = s.toCharArray();
+
+        for (int i = charArray.length - 1; i >= 0; i--) {
+            char currentChar = charArray[i];
+
+            while (currentChar >= 'A' && currentChar <= 'Z' || currentChar >= 'a' && currentChar <= 'z') {
+                lastWordLength++;
+                if (i == 0) break;
+                currentChar = charArray[--i];
+            }
+
+            if (lastWordLength != 0) break;
+        }
+
+        return lastWordLength;
+    }
+
+    public static void main(String[] args) {
+        String s = " a";
+        int i = lengthOfLastWord(s);
+        System.out.println(i);
+    }
+}
